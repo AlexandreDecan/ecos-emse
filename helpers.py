a5cd93028ef17074231636cc2494e16ffb7260ab
Alexandre Decan <alexandre.decan@lexpage.net>
2017-05-05 09:09:14 +0200

    Gini / Lorenz for required packages

--- helpers.py
+++ helpers.py
@@ -341,7 +341,24 @@ def x_index(values):
             return d
     return 0
     
+
+def gini(array):
+    """Calculate the Gini coefficient of a numpy array."""
+    # based on bottom eq: http://www.statsdirect.com/help/content/image/stat0206_wmf.gif
+    # from: http://www.statsdirect.com/help/default.htm#nonparametric_methods/gini.htm
+    # Taken from https://github.com/oliviaguest/gini
+    import numpy as np
     
+    array = array.flatten() #all values are treated equally, arrays must be 1d
+    if np.amin(array) < 0:
+        array -= np.amin(array) #values cannot be negative
+    array += 0.0000001 #values cannot be 0
+    array = np.sort(array) #values must be sorted
+    index = np.arange(1,array.shape[0]+1) #index per array element
+    n = array.shape[0]#number of array elements
+    return ((np.sum((2 * index - n  - 1) * array)) / (n * np.sum(array))) #Gini coefficient
+    
+
 if __name__ == '__main__':
     for ecosystem in ECOSYSTEMS:
         print(ecosystem)
